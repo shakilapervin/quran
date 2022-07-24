467d8b13ce823963b3a499f65d1e7e3b5f765d7a
Emma <we@else.co.il>
2022-07-24 14:04:16 +0600

    New Update

--- .next/server/chunks/652.js
+++ .next/server/chunks/652.js
@@ -203,7 +203,7 @@ const session = {
     cookieName: "sm-pos",
     password: "s%3Al3ozSdvQ83TtC5RvJ.CibaQoHtaY0H3QOB1kqR8H2A",
     cookieOptions: {
-        secure: true
+        secure: false
     }
 };
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (session);
